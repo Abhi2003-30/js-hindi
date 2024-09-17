2aeb1f9bc52085025a50c2f2efd1c84645a0b6f3
Abhi2003-30 <ac9805412188@gmail.com>
2024-09-17 11:06:47 +0000

    variables config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "abhishek@google.com"
+var accountPassword = "12345"
+accountCity = "Himachal Pradesh"
+let accountState;
+
+// accountId = 2
+accountEmail = "anhi@gmail.com"
+accuntPassword = "121211212"
+accountCity = "bengaluru"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
