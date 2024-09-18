8f621db6f5d2305b8a201d9d597865075e8aa821
Abhi2003-30 <ac9805412188@gmail.com>
2024-09-18 03:57:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as a newr version
+
+//alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3)//code readability should be high
+
+console.log("Abhishek")
+
+
+let name = "Abhishek"
+let age = 18
+let isloggedIn = false
+let state = null
+// number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => it is used for unique
+
+// object
+
+console.log(typeof "abhishek");
