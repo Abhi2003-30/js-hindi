166835d7d61ac8f208d446f6508bf80f79466dd2
Abhi2003-30 <ac9805412188@gmail.com>
2024-10-04 06:57:26 +0000

    objects in js 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -19,8 +19,37 @@ const regularUser = {
 
 // console.log(regularUser.fullname?.userfullname.firstname);
 
-const objs1 = {1: "a", 2: "b"}
-const objs2 = {3: "c", 4: "d"}
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj4 = {5: "e", 6: "f"}
 
-const obj3 = {objs1, objs2}
-console.log(obj3);
+// const obj3 = {objs1, objs2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "abhi@gmail.com",
+    },
+    {
+        id: 1,
+        email: "abhi@gmail.com",
+    },
+    {
+        id: 1,
+        email: "abhi@gmail.com",
+    },
+]
+
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('name'));
+console.log(tinderUser.isPrototypeOf('name'));
