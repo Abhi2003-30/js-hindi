a98c4f502d6345d8bef3acf6cfdb3899fefc4f36
Abhi2003-30 <ac9805412188@gmail.com>
2024-10-07 04:39:38 +0000

    Scopes in js

---
+++ 03_basics/02_scops.js
@@ -0,0 +1,50 @@
+// var c = 300
+let a = 300
+
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER:", a);
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Abhishek"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+     two()
+}
+
+// one()
+
+if (true) {
+    const username = "Abhishek"
+    if (username === "Abhishek") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++++ intresting ++++++++++++++++++++++++++
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+console.log(addtwo(5));
+const addtwo = function(num){
+    return num + 2
+}
