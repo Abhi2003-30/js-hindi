a43809d636c4c6476f511e043ecd7f25a5373bf1
Abhi2003-30 <ac9805412188@gmail.com>
2024-10-12 09:25:37 +0000

    loop four

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js", "c++", "py", "java", "rb"]
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "united state of america")
+// map.set('Fr', "France")
+
+// for (const key in map) {
+//      console.log(key);
+     
+// }
+
